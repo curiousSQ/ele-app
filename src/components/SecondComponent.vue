f1c231af27dda234eeca301477e0e9c70bc34943
Sunqian <Sunqian@gridsum.com>
2018-06-12 18:55:55 +0800

    搭建vue环境

---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  name: 'SecondComponent',
+  data () {
+    return {
+      msg: 'something'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/goods.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'goods',
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
